315200b41677f9690af42dc1668b9c87e92b99c7
FelipepPrado <felipepdelimaa@gmail.com>
2025-01-14 10:25:49 -0300

    Update main.py

--- FletProjeto/src/main.py
+++ FletProjeto/src/main.py
@@ -7,17 +7,47 @@ class MyButton(ft.ElevatedButton):
         super().__init__()
         self.bgcolor = ft.Colors.GREEN
         self.color = ft.Colors.BLACK
-        self.text = "Entrar"
+        self.content= ft.Row([
+                            ft.Icon(name=ft.icons.LOGOUT, color=ft.Colors.BLACK),
+                            ft.Text("Entrar"),
+                        ],
+                        width=285,
+                        alignment=ft.MainAxisAlignment.CENTER
+                    )
 
 def main(page: ft.Page):
     page.title = "Planting The Future"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    email_user = ft.TextField(label = "Email", width=300)
-    senha_user = ft.TextField(label="Senha", password=True, can_reveal_password=True, width=300)
+    email_user = ft.TextField(
+        label = "Email",
+        width=300,
+        bgcolor = ft.colors.WHITE,
+        border_radius=8,
+        border_width=0
+        )
+    
+    senha_user = ft.TextField(
+        label="Senha", password=True,
+        can_reveal_password=True,
+        width=300,
+        bgcolor = ft.colors.WHITE,
+        border_radius=8,
+        border_width=0
+        )
+    
     page.add(
-        email_user,
-        senha_user,
-        MyButton()
+        ft.Container(
+            alignment=ft.alignment.center,
+            content=ft.Column( 
+                [
+                    ft.Container(email_user, border=ft.border.all(color="#4A3928", width=2), border_radius=10),
+                    ft.Container(senha_user, border=ft.border.all(color="#4A3928", width=2), border_radius=10),
+                    ft.Container(MyButton(), border=ft.border.all(color="#000000", width=2), border_radius=40)
+                ]
+            )
+                
+        )
     )
 
 

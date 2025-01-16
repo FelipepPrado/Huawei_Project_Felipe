9a26bf20cb0954a1762e268241bc887062920de9
FelipepPrado <felipepdelimaa@gmail.com>
2025-01-15 22:46:26 -0300

    Update main.py

--- FletProjeto/src/main.py
+++ FletProjeto/src/main.py
@@ -1,25 +1,12 @@
 import flet as ft
 
-#Forma de criar um botão personalizado, vou precisar pra um botão do login, então é bom
-class MyButton(ft.ElevatedButton):
-    def __init__(self):
-        super().__init__()
-        self.bgcolor = ft.Colors.GREEN
-        self.color = ft.Colors.BLACK
-        self.style = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=7))
-        self.content=ft.Row([
-                            ft.Icon(name=ft.icons.LOGOUT, color=ft.Colors.BLACK),
-                            ft.Text("Entrar")
-                        ],
-                        width=300,
-                        height=28,
-                        alignment=ft.MainAxisAlignment.CENTER
-                    )
-
 def main(page: ft.Page):
     Brown = "#4A3928"
     page.title = "Planting The Future"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    def button_clicked():
+        pass
 
     image1 = ft.Image(
         src="C:/Users/Felipe/Downloads/Floresta Urbana 1 (1).png",
@@ -31,7 +18,7 @@ def main(page: ft.Page):
     image2 = ft.Image(
         src="C:/Users/Felipe/Downloads/image 1.png",
         width=430,
-        height=410,
+        height=400,
         #border_radius= ft.border_radius.only(top_left, top_right),
         fit=ft.ImageFit.COVER,
     )
@@ -71,7 +58,7 @@ def main(page: ft.Page):
                 image2,
                 ft.Container(
                     width=430,
-                    height=360,
+                    height=380,
                     bgcolor = "#E7E1E1",
                     border_radius=15,
                     content=ft.Column(
@@ -87,8 +74,24 @@ def main(page: ft.Page):
                             email_user,
                             senha_user,
                             ft.TextButton(text = "Esqueceu sua senha?", style = ft.ButtonStyle(color = "#000000")),
-                            ft.Container(MyButton(), border=ft.border.all(color="#000000", width=2), border_radius=8)
-                                
+                            ft.Container(
+                                        ft.ElevatedButton(
+                                           bgcolor = ft.Colors.GREEN,
+                                           color = ft.Colors.BLACK,
+                                           style = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=7)),
+                                           content=ft.Row([
+                                                    ft.Icon(name=ft.icons.LOGOUT, color=ft.Colors.BLACK),
+                                                    ft.Text("Entrar")
+                                                ],
+                                                width=300,
+                                                height=28,
+                                                alignment=ft.MainAxisAlignment.CENTER
+                                            )
+                                        ),
+                                        border=ft.border.all(color="#000000", width=2),
+                                        border_radius=8,
+                                        on_click=button_clicked
+                            )   
                         ],
                         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                         #alignment=ft.MainAxisAlignment.END
@@ -96,8 +99,7 @@ def main(page: ft.Page):
                 )
             ],
             spacing=0
-        ),
-    
+        )
     )
 
 
